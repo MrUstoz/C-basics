dbfcb379b37b7d75696554b2ef5169aa79a63553
Tahsimuz <165240557+Tahsimuz@users.noreply.github.com>
2024-04-06 10:03:12 +0500

    List & Object

--- Class_Object/main.cpp
+++ Class_Object/main.cpp
@@ -41,8 +41,143 @@ public:
     }
 };*/
 
+/*
+class Person
+{
+public:
+    string Fullname;
+    string Address;
+    int Age;
+
+
+    Person(string f, string a, int e) : Fullname(f), Address(a), Age(e) {}
+
+    void Print()
+    {
+        cout << Fullname << " " << Address << " " << Age << endl;
+    }
+};*/
+
+class User
+{
+public:
+    string Username;
+    string Email;
+    int Password;
+};
+
+class UserService{
+public:
+    list<User> Users;
+
+    string AddUser(User newUser)
+    {
+        for(auto user : Users)
+        {
+            if(user.Username == newUser.Username)
+            {
+                return "Bunday foydalanuuvchi mavjud!";
+            }
+
+            if(user.Email == newUser.Email)
+            {
+                return "Bunday email orqali avval ro'yxatdan o'tilgan";
+            }
+        }
+
+        Users.push_back(newUser);
+        return "Successfully saved";
+    }
+
+    void GetUsers()
+    {
+        for(auto& user : Users)
+        {
+            cout << "Username: " << user.Username << ", Email: " << user.Email << ", Password: " << user.Password << "\n";
+        }
+    }
+};
+
+
+
+
+
 int main()
 {
+    cout << "Welcome to out!";
+    UserService service;
+
+    int n, p;
+    string u, e;
+
+    for(int i = 0; i < 5; i++)
+    {
+        cout << "\n 1-Register\n 2-Get User\n 3-Close\n Input: ";
+        cin >> n;
+        if(n == 1)
+        {
+            cout << "\nUsername: ";
+            cin >> u;
+            cout << "\nEmail: ";
+            cin >> e;
+            cout << "\nPassword: ";
+            cin >> p;
+
+            User newUser;
+
+            newUser.Username = u;
+            newUser.Email = e;
+            newUser.Password = p;
+
+            string res = service.AddUser(newUser);
+            cout << res;
+        }
+        else if(n == 2)
+        {
+            cout << "Users information: ";
+            service.GetUsers();
+        }
+        else if(n == 3)
+        {
+            break;
+        }
+        else
+        {
+            cout << "Bunaqa bolim mavjud emas!\n";
+        }
+    }
+
+    return 0;
+    /*
+    list<User> Users;
+
+    User u1("foziljonov7", "abdulvosidfoziljonov55@gmail.com", 1234);
+    User u2("FAA", "asjhbsabdah@gmail.com", 1234);
+
+    Users.push_back(u1);
+    Users.push_back(u2);
+
+    for(auto& user : Users)
+    {
+        user.Print();
+    }*/
+
+
+    /*
+    list<Person> person;
+
+    Person p1("Abdulvosid Foziljonov", "Fergana", 19);
+    Person p2("Nodirbek Abdulaxadov", "Fergana", 22);
+
+    person.push_back(p1);
+    person.push_back(p2);
+
+    for(auto& people : person)
+    {
+        people.Print();
+    }*/
+
+
     /*Animal animal;
     animal.SetName("Cat");
     animal.GetVoice("Miow");
@@ -88,13 +223,13 @@ int main()
     }
 */
 
-    list<string> fruits {"Olma", "Nok", "Uzum"};
+   /* list<string> fruits {"Olma", "Nok", "Uzum"};
 
     list<bool> isPublic {true, false, false};
 
     cout << (isPublic.front() == true) << endl;
 
-    cout << (isPublic.end() == isPublic.begin()) << endl;
+    cout << (isPublic.end() != isPublic.begin()) << endl;*/
 
     /*for(bool is : isPublic)
     {
@@ -108,10 +243,9 @@ int main()
         }
     }*/
 
-    for(string fruit : fruits)
+    /*for(string fruit : fruits)
     {
         cout << fruit << endl;
-    }
+    }*/
 
-    return 0;
 }
